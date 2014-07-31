7aacff6dc001639f6f95ac4d7bcd39ec71dd07a3
Shinmera <shinmera@tymoon.eu>
2014-07-31 20:25:53 +0200

    Exports

--- package.lisp
+++ package.lisp
@@ -8,4 +8,28 @@
 (defpackage #:clip
   (:nicknames #:org.tymoonnext.clip)
   (:use #:cl #:lquery)
-  )
+  ;; attr-processors.lisp
+  (:export
+   #:process-attribute
+   #:define-attribute-processor
+   #:process-attributes)
+  ;; clipboard.lisp
+  (:export
+   #:*clipboard*
+   #:clip
+   #:clipboard
+   #:make-clipboard
+   #:resolve-value
+   #:resolve-attribute)
+  ;; processor.lisp
+  (:export
+   #:*target*
+   #:process)
+  ;; tag-processors.lisp
+  (:export
+   #:process-tag
+   #:define-tag-processor
+   #:process-children
+   #:process-node)
+  ;; toolkit.lisp
+  (:export))
