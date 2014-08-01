dd01627a8134f20afa3812d00eb9addf0b91748c
Shinmera <shinmera@tymoon.eu>
2014-08-01 08:42:47 +0200

    Added SPLICE, WHEN, UNLESS, IF

--- tag-processors.lisp
+++ tag-processors.lisp
@@ -20,7 +20,9 @@
                                   (cons (pop args) (make-body))))))
 
 (defun process-children (node)
-  (loop for child across (plump:children node)
+  (loop for i from 0 ;; We do this manually to allow growing size of the array.
+        while (< i (length (plump:children node)))
+        for child = (aref (plump:children node) i)
         do (process-node child))
   node)
 
@@ -30,6 +32,7 @@
   node)
 
 (defun process-node (node)
+  (incf *target-counter*)
   (let ((*target* node))
     (etypecase node
       (plump:element (process-tag (make-keyword (string-upcase (plump:tag-name node))) node))
@@ -58,4 +61,65 @@
   (plump:remove-attribute node "to")
   (process-node node))
 
-;; Todo: Tag splicing, conditionals
+(defun splice-into (node pos new-children)
+  (let ((family (plump:children node))
+        (childcount (length new-children)))
+    (when (< 0 childcount)
+      (plump::array-shift family :n (1- childcount) :from pos)
+      (loop for i from 0 below childcount
+            for child = (aref new-children i)
+            do (setf (plump:parent child) node
+                     (aref family (+ pos i)) child)))))
+
+(define-tag-processor splice (node)
+  (process-attributes node)
+  (let* ((first (plump:first-element node))
+         (parent (plump:parent node))
+         (family (plump:children parent))
+         (pos (position node family))
+         (childcount (length (plump:children node))))
+    (when first (process-node first))
+    (cond
+      ((< 0 childcount)
+       (splice-into parent pos (plump:children node))
+       (setf (plump:parent node) NIL))
+      (T (plump:remove-child node)))))
+
+(define-tag-processor when (node)
+  (process-attributes node)
+  (let ((test (resolve-value (read-from-string (plump:attribute node "test")))))
+    (if test
+        (process-tag :splice node)
+        (plump:remove-child node))))
+
+(define-tag-processor unless (node)
+  (process-attributes node)
+  (let ((test (resolve-value (read-from-string (plump:attribute node "test")))))
+    (if test
+        (plump:remove-child node)
+        (process-tag :splice node))))
+
+(define-tag-processor if (node)
+  (process-attributes node)
+  (let* ((test (resolve-value (read-from-string (plump:attribute node "test"))))
+         (parent (plump:parent node))
+         (pos (position node (plump:children parent)))
+         (then) (else))
+    (loop for child across (plump:children node)
+          when (plump:element-p child)
+            do (when (and (not then) (string-equal (plump:tag-name child) "then"))
+                 (setf then child))
+               (when (and (not else) (string-equal (plump:tag-name child) "else"))
+                 (setf else child)))
+    (flet ((splice (children)
+             (when (< 0 (length children))
+               (splice-into parent pos children)
+               (process-node (aref children 0)))))
+      (cond
+        ((and then test)
+         (when else (plump:remove-child else))
+         (splice (plump:children then)))
+        (else
+         (when then (plump:remove-child then))
+         (splice (plump:children else)))
+        (T (plump:remove-child node))))))
